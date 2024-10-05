df9c625377774dfe2255f1c5e72758a0d48b0c00
MrAkbari5931 <daniakbari13878713@gmail.com>
2024-10-05 18:48:12 +0330

    Update app.js

--- javaScript/app.js
+++ javaScript/app.js
@@ -139,7 +139,7 @@ function mainPageSources() {
     let footerBottom = $.body.scrollHeight - footer.scrollHeight
     footer.style.top = footerBottom + 'px'
 
-const productsToShow = productsList.slice(0, 10); // انتخاب 2 محصول اول
+const productsToShow = productsList.slice(0, 14); // انتخاب 14 محصول اول
 
 productsToShow.forEach(function(product) {
     const productBox = $.createElement('div');
